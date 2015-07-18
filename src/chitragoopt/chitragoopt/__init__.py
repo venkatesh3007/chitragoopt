1623da195f3165aca0712ed8ed818d27cbf0544d
Venkatesh Rao <venkatesh@baniyafy.com>
2015-07-19 00:00:27 +0530

    Initial Commit

---
+++ src/chitragoopt/chitragoopt/__init__.py
@@ -0,0 +1,22 @@
+'''
+chitragoopt: Main module
+
+Copyright 2015, Venkatesh Rao
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'chitragoopt' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
